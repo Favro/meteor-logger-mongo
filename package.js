0225e19c5d8c768dc17c8655b905adf7306eb1ab
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-10-12 12:59:07 +0200

    spaces -> tabs

--- package.js
+++ package.js
@@ -3,24 +3,24 @@ Package.describe({
 	git: 'https://github.com/hansoft/meteor-logger-mongo.git',
 	name: 'hansoft:logger-mongo',
 	summary: 'Store console.<log/warn/error> in mongodb.',
-  version: '1.0.0',
+	version: '1.0.0',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2');
+	api.versionsFrom('1.2');
 
-  api.use([
+	api.use([
 		'check',
 		'ecmascript',
 		'mongo',
 		'underscore',
 	]);
 
+	api.use('insecure', ['client', 'server'], { weak: true });
+
 	api.use([
 		'dburles:mongo-collection-instances',
 	]);
-
-	api.use('insecure', ['client', 'server'], { weak: true });
 
 	api.addFiles('lib/logger-mongo.js');
 	api.addFiles('server/logger-mongo.js', 'server');
